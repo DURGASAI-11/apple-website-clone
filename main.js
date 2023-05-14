9da6c1afd73122e3b8d247ff811443e4ec9d0e17
DURGASAI-11 <85279734+DURGASAI-11@users.noreply.github.com>
2023-05-14 22:09:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+/*
+window.addEventListener('scroll',reveal);
+function reveal()
+{
+    var reveals =document.querySelectorAll('.reveal')
+    for(var i=0;i<reveals.length;i++)
+    {
+        var wH=window.innerHeight;
+        var windowElementTop=reveals[i].getBoundingClientRect().top;
+        var elementVisible=150;
+
+        if(windowElementTop < wH-elementVisible)
+        {
+            reveals[i].classList.add('active');
+        }
+        else
+            {
+                reveals[i].classList.remove('active');
+            }
+        
+    }
+}
+*/
